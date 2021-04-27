48448b493a8c31a71d1a6fc49d599257718a9883
mamunabdullah <mamundigitalride@gmail.com>
2021-04-27 23:06:06 +0600

    Initial Commit

---
+++ evalyDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  evalyDemoApp
+//
+//  Created by Abdullah Al Mamun on 27/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
